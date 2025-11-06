98e371e1b666b14be4fba3d64b937a788ac8c88c
Banjer71 <davgio7@gmail.com>
2025-11-06 12:41:36 +0000

    fixing issue on next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+  images: {
+    unoptimized: true, // ✅ disable image optimization for CI
+  },
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
+  webpack(config) {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
